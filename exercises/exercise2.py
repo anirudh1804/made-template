3cf288595d6c0b264f2b44ff98e79987ef9e5d46
Aniruddha Maiti <aniruddh.maiti@gmail.com>
2023-11-22 01:05:49 +0100

    final

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -3,33 +3,33 @@ import io
 import re
 from sqlalchemy import create_engine
 
-# Step 1: Train stops data Extraction from provided CSV File
+#exttract train data from csv file
 csv_url = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
 
-
-# Step 2: Train stops CSV Data Transformation
+# transform and seperate the data
 df = pd.read_csv(csv_url, delimiter=';')
 
-# Requirement 1: Drop the "Status" column
+#drop the status column
 del df["Status"]
 
-# Requirement 2: Drop rows with invalid values
+#sort data
+#keep valid data
 valid_verkehr = {"FV": None, "RV": None, "nur DPN": None}
 
-# Requirement 3: Drop rows with invalid values
+# drop invalid values
 df = df.dropna(axis=0, how='any')  # Drop rows with any empty cells
 df = df.query("Verkehr in @valid_verkehr")
 
-# Requirement 4: Validate IFOPT values based on the specified pattern
+#Validate IFOPT values 
 pattern = re.compile(r"^\w{2}:\d+:\d+(?::\d+)?$")
 valid_ifopt = df["IFOPT"].astype(str).str.contains(pattern, regex=True)
 df = df.loc[valid_ifopt]
 
 # Replacing , with . 
-
 df['Laenge'] = df['Laenge'].str.replace(r',', '.')
 df['Breite'] = df['Breite'].str.replace(r',', '.')
 
+#geography coordinates system 
 df['Laenge'] = df['Laenge'].astype(float)
 df = df.query("-90 <= Laenge <= 90")
 
@@ -37,8 +37,7 @@ df['Breite'] = df['Breite'].astype(float)
 df = df.query("-90 <= Breite <= 90")
 
 
-# Step 3: Data Loading
-#db_engine = create_engine("sqlite:///trainstops.sqlite")
+#create the table
 table_name = "trainstops"
 
 # Define column data types
@@ -56,5 +55,5 @@ column_types = {
     
 }
 df = df.astype(column_types)
-# Create table and insert data
+# insert data into sqlite
 df.to_sql(table_name, 'sqlite:///trainstops.sqlite', if_exists="replace", index=False)
