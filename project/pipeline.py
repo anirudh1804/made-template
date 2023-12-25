9ef19f969b7979125c433692c5d00293727b80db
anirudh1804 <119449086+anirudh1804@users.noreply.github.com>
2023-12-25 13:57:54 +0100

    Update pipeline.py

--- project/pipeline.py
+++ project/pipeline.py
@@ -7,49 +7,46 @@
 #import data for temperatures
 
 import pandas as pd
-tempdf=pd.read_csv(r"D:\FAU\Semester 3\MADE\Datasets\temp\temperatures.csv")
+tempdf=pd.read_csv("../data/temperatures.csv")
 tempdf.head()
 
 #remove unwanted columns from 1901 until 2009
 #sort the dataset in ascending order of year - keep data from last decade
 #remove rows before 2010
-tempdf = tempdf.sort_values("YEAR", ascending=False)
-tempdf = tempdf[0:8]
+tempdf.rename(columns={'YEAR': 'Year'}, inplace=True)
+df = tempdf.sort_values("Year", ascending=False)
+new_df = df[0:8]
 #remove columns with quarterly records
-tempdf = tempdf.drop(tempdf.loc[:, 'JAN-FEB':'OCT-DEC'].columns, axis=1)
-tempdf.head(20)
-
-
-selected_columns = tempdf[['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']]
-
-# Add the values of the data across the selected columns
-#average temperature through 2010 to 2017
-tempdf["Avg Temp"] = selected_columns.sum(axis=1)/8
-tempdf.head(20)
-
+df1 = new_df.drop(new_df.loc[:, 'JAN-FEB':'OCT-DEC'].columns, axis=1)
+df1.head(20)
 
 # In[5]:
 
-
-
-#import data for CO2 emissions
+#import co2 emissions
 
 import pandas as pd
-codf = pd.read_csv(r"D:\FAU\Semester 3\MADE\Datasets\co2_emission.csv")
-codf.head()
-
+import matplotlib.pyplot as plt
+df2 = pd.read_csv("../data/co2_emission.csv")
+df2.head()
 #sort the data
-codf = codf.sort_values(by='Entity', ascending=True)
+df2 = df.sort_values(by='Entity', ascending=True)
 #only select the data for India between 2010 and 2017
-codf = codf[(codf["Entity"] == "India") & (codf["Year"] >= 2010)]
-codf = codf.drop(['Code'], axis=1)
+df2 = df[(df["Entity"] == "India") & (df["Year"] >= 2010)]
+df2 = df.drop(['Code'], axis=1)
 # Rename the 'Entity' column to 'Country'
-codf.rename(columns={'Entity': 'Country'}, inplace=True)
-codf.head(10)
+df2.rename(columns={'Entity': 'Country'}, inplace=True)
+df2.rename(columns={'Annual CO₂ emissions (tonnes )': 'emmissionslarge'}, inplace=True)
+df2.head(10)
 
+# Assuming you have two DataFrames: temperature_df and co2_emissions_df
 
-# In[ ]:
+# Merge the two DataFrames
+merged_df = pd.merge(df1, df2, left_on='ANNUAL', right_on='emmissionslarge', how='inner')
 
+# Save the merged DataFrame to an SQLite file
+sqlite_file_path = 'merged_data.db'
+merged_df.to_sql('merged_data', sqlite_file_path, index=False, if_exists='replace')
 
+print("Merged data saved to SQLite file:", sqlite_file_path)
 
 
