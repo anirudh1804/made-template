32fb7f56e4c6c8517df79b76160324adb08f503c
anirudh1804 <119449086+anirudh1804@users.noreply.github.com>
2023-12-25 22:49:50 +0100

    Update tests.py

--- project/tests.py
+++ project/tests.py
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-# Exit on error
-set -e
-
 # Define paths
 data="pipeline.py"
 
 # Validate that the Python script exists
-if [ -e "$data" ]; then
-    echo "Test passed, Python script exists."
+if data:
+    print("Test passed, Python script exists.")
 else
-    echo "Test failed, Python script does not exist."
+    print("Test failed, Python script does not exist.")
     exit 1
-fi
 
 sqlite="merged_data.sqlite"
-if [ -e "$sqlite" ]; then
-    echo "Test passed: SQLite file exists."
+if sqlite:
+    print("Test passed: SQLite file exists.")
 else
-    echo "Test failed: SQLite file does not exist."
+    print("Test failed: SQLite file does not exist.")
     exit 1
-fi
 
 # Test passed
-echo "All tests passed!"
+print("All tests passed!")
 exit 0
